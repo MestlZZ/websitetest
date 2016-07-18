cfdbe3fbdc192341430e2ce147ee8d60a738e3f7
b.herasymov <b.herasymov@DEV-UA-WS154>
2016-07-18 09:31:50 +0300

    bootstrapper + bower

---
+++ EasyPlan.Web/App/bootstrapping/extraJsTask.js
@@ -0,0 +1,12 @@
+﻿define(function (reqiure) {
+    return {
+        initialize
+    }
+
+    function initialize() {
+        return Q.all([
+            require('packages/handlebars-4.0.5'),
+            require('packages/underscore')
+        ])
+    }
+})
