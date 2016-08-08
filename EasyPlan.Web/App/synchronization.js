86df8aea7850639fac6798da38be9f0fa7e6f688
b.herasymov <b.herasymov@DEV-UA-WS154>
2016-08-08 13:46:58 +0300

    fix bug with hub works

--- EasyPlan.Web/App/synchronization.js
+++ EasyPlan.Web/App/synchronization.js
@@ -1,6 +1,8 @@
 ﻿define(['durandal/app', 'constants'], function (app, constants) {
 
     var boardHub = $.connection.boardHub;
+    var boardId = '';
+    var isConnectedToBoard = false;
 
     boardHub.client.updateItemTitle = function (id, title) {
         app.trigger(constants.EVENT.BOARD.ITEM.TITLE_CHANGED, id, title);
@@ -47,11 +49,20 @@
     }
 
     function openBoard(id) {
+        isConnectedToBoard = true;
+        boardId = id;
+
         boardHub.server.openBoard(id);
     }
 
     function closeBoard() {
-        boardHub.server.closeBoard();
+        if (_.isEmpty(boardId))
+            throw 'Can not disconect from board because board id is clear';
+
+        boardHub.server.closeBoard(boardId);
+
+        isConnectedToBoard = false;
+        boardId = '';
     }
 
     function connect() {
