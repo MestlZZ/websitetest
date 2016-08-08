68ecf499727a078dcc37409487716201de31c1cb
b.herasymov <b.herasymov@DEV-UA-WS154>
2016-08-08 15:31:29 +0300

    code style fix

---
+++ EasyPlan.Web/App/core/packages/underscore/underscore.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    _.mixin({
+        isInvalidText: function (text) {            
+            if (_.isNull(text) || _.isUndefined(text) || _.isEmpty(text)) {
+                return true;
+            }
+
+            return false;
+        },
+        isInvalidNumber: function (number) {
+            if (_.isNull(number) || _.isUndefined(number) || _.isNaN(number) || !_.isFinite(number)) {
+                return true;
+            }
+
+            return false;
+        }
+    })
+})();
